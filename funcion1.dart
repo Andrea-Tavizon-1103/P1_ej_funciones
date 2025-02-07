e878aa38871f9ddda09db5cde8e6d0bb3cd9ad57
Andrea Tavizon <164534025+Andrea-Tavizon-1103@users.noreply.github.com>
2025-02-07 16:47:38 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+
+// funcion que no devuelve valor
+// funcion saludar
+void saludar(){
+  print('hola chat');
+}
+
+void escribeNombre(){
+  print('Andrea Tavizon Torres');
+}
+
+void main(){
+  // Andrea Tavizon Torres 22308051281103
+  // llamamos a la funcion saludar
+  print('Andrea Tavizon Torres 22308051281103');
+  print('Llamamos a la funcion saludar');
+  saludar();
+  print('Llamamos a la funcion escribeNombre');
+  escribeNombre();
+}
