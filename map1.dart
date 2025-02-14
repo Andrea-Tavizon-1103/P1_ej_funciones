e54cdfeafbc16cebf053bbd255780cc1ec1045e9
Andrea Tavizon <164534025+Andrea-Tavizon-1103@users.noreply.github.com>
2025-02-14 16:19:54 +0000

    mapa ejemplo

---
+++ map1.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Crear un mapa con tipo <int, String> para los meses
+  Map<int, String> meses = {
+    1: 'Enero',
+    2: 'Febrero',
+    3: 'Marzo',
+    4: 'Abril',
+    5: 'Mayo',
+    6: 'Junio',
+    7: 'Julio',
+    8: 'Agosto',
+    9: 'Septiembre',
+    10: 'Octubre',
+    11: 'Noviembre',
+    12: 'Diciembre',
+  };
+
+  // Acceder a los valores del mapa
+  print('El mes 1 es: ${meses[1]}'); // Enero
+  print('El mes 6 es: ${meses[6]}'); // Junio
+
+  // Recorrer el mapa e imprimir todos los pares clave-valor
+  print('\nLista completa de meses:');
+  //usando forEach
+  meses.forEach((numero, nombre) {
+    print(' $nombre');
+  });
+}
